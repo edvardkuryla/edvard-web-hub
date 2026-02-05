c30cad23c9cebab6f5472f8c0f3ffc18cb959eb1
Edvard <edvardkuryla@gmail.com>
2026-02-05 14:11:36 +0100

    added: authservice

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.core.database import Base
 from alembic import context
-from app.users import models
+from app.models import models
 
 load_dotenv()
 
--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from app.core.database import Base
 
 class User(Base):
@@ -8,3 +8,6 @@ class User(Base):
     name = Column(String, unique=False, nullable=False, index=True)
     email = Column(String, unique=False, nullable=False, index=True)
     password_hash = Column(String, nullable=False)
+
+    role = Column(String, default="user") # user | admin
+    is_active = Column(Boolean, default=True)
