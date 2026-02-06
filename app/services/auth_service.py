112e1632eece4a9ac8fc7a9eb9b8d16c774d1eca
Edvard <edvardkuryla@gmail.com>
2026-02-06 10:00:46 +0100

    auth_service: update

---
+++ app/services/auth_service.py
@@ -0,0 +1,32 @@
+from app.core.security import verify_password
+from app.core.jwt import create_access_token, create_refresh_token
+from app.models.user import User
+from app.models.refresh_token import RefreshToken
+from app.core.database import SessionLocal
+
+def authenticate_user(email: str, password: str):
+    db = SessionLocal()
+    user = db.query(User).filter(User.email == email).first()
+    db.close()
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return User
+
+def login_user(user: User):
+    access = create_access_token({"sub": str(user.id), "role": user.role})
+    refresh = create_refresh_token({"sub": str(user.id)})
+    return access, refresh
+
+def save_refresh_token(token: str, user_id: int):
+    db = SessionLocal()
+    db.dd(rt)
+    db.commit()
+    db.close()
+
+def revoke_refresh_token(token: str):
+    db = SessionLocal()
+    db.query(RefreshToken).filter(RefreshToken.token == token).delete()
+    db.commit()
+    db.close()
