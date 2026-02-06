8de82d58de65c525a4f145ba2f538ea9910488b2
Edvard <edvardkuryla@gmail.com>
2026-02-06 09:31:42 +0100

    jwt update

--- app/core/security.py
+++ app/core/security.py
@@ -1,19 +1,14 @@
-from passlib.context import CryptContext
-from datetime import datetime, timedelta, timezone
-from jose import jwt, JWTError
-from ..core.config import settings
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+ph = PasswordHasher()
 
-def get_password_hash(password) -> str:
-    return pwd_context.hash(password)
+def hash_password(password: str) -> str:
+    return ph.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
-    to_encode.update({"exp": expire})
-
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+def verify_password(password: str, hashed: str) -> bool:
+    try:
+        ph.verify_password(hashed, password)
+        return True
+    except VerifyMismatchError:
+        return False
